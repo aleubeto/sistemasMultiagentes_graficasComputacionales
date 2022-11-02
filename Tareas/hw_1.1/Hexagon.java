e71de4e92b1f790abbde5576b9307700d3994624
Omar Jimenez <omar_ji_armen@hotmail.com>
2022-11-01 20:10:16 -0600

    HW_1.1: Rectangle, Diamond, Hexagon, Octagon added

---
+++ Tareas/hw_1.1/Hexagon.java
@@ -0,0 +1,38 @@
+class Hexagon{
+    public static void main(String[] args){
+        int nTamano = Integer.valueOf(args[0]);
+        linea(nTamano);
+        for (int i = 1; i < nTamano; i++){
+            for (int j = 1; j < nTamano - i; j++){
+                System.out.print(" ");
+            }
+            System.out.print("+");
+            for (int j = 0; j < 2 * i + 2 * nTamano - 3; j++){
+                System.out.print(" ");
+            }
+            System.out.print("+\n");
+        }
+
+        for (int i = 1; i < nTamano - 1; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            System.out.print("+");
+            for (int j = 0; j < 2 * nTamano - 2 * i + 2 * nTamano - 5; j++){
+                System.out.print(" ");
+            }
+            System.out.print("+\n");
+        }
+        linea(nTamano);
+    }
+
+    private static void linea (int nTamano) {
+        for (int i = 0; i < nTamano - 1; i++){
+            System.out.print(" ");
+        }
+        for (int i = 0; i < nTamano; i++){
+            System.out.print("+ ");
+        }
+        System.out.print('\n');     
+    }
+}
---
+++ Tareas/hw_1.1/Rectangle.java
@@ -0,0 +1,22 @@
+class Rectangle{
+    public static void main(String[] args){
+        int nFilas = Integer.valueOf(args[0]);
+        int nColumnas = Integer.valueOf(args[1]);
+        linea(nColumnas);
+        for (int i = 0; i < nFilas - 2; i++){
+            System.out.print("+ ");
+            for (int j = 0; j < nColumnas - 2; j++){
+                System.out.print("  ");
+            }
+            System.out.print("+\n");  
+        }
+        linea(nColumnas);
+    }
+
+    private static void linea (int nTamano) {
+        for (int i = 0; i < nTamano; i++){
+            System.out.print("+ ");
+        }
+        System.out.print('\n');     
+    }
+}
