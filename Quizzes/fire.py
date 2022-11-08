9ece542804e727f497f3a4d0c8b71d9daf6ce88a
ale_wsl <alejo.ubeto@gmail.com>
2022-11-07 18:15:21 -0600

    P3 implementación de gráfica de arboles quemados

--- Quizzes/fire.py
+++ Quizzes/fire.py
@@ -7,6 +7,9 @@ from mesa.visualization.ModularVisualization import ModularServer
 
 from mesa.visualization.UserParam import UserSettableParameter
 
+from mesa.datacollection import DataCollector
+from mesa.visualization.modules import ChartModule
+
 # Clase Agente: Arbol
 class Tree(Agent):
     FINE = 0
@@ -36,9 +39,22 @@ class Forest(Model):
                     tree.condition = Tree.BURNING
                 self.grid.place_agent(tree, (x,y))
                 self.schedule.add(tree)
+            
+        # Recolector de información: procentaje de arboles quemados
+        self.datacollector = DataCollector({"Percent burned": lambda m: self.count_type(m, Tree.BURNED_OUT) / len(self.schedule.agents)})
+
+    # Método estático para referenciar al modelo Forest dentro de la función lambda
+    @staticmethod
+    def count_type(model, condition):
+        count = 0
+        for tree in model.schedule.agents:
+            if tree.condition == condition:
+                count += 1
+        return count
 
     def step(self):
         self.schedule.step()
+        self.datacollector.collect(self)
 
 # Coloreado de agentes
 def agent_portrayal(agent):
@@ -55,11 +71,15 @@ def agent_portrayal(agent):
 
 grid = CanvasGrid(agent_portrayal, 50, 50, 450, 450)
 
-# Slider para manipular densidad de bosque
-server = ModularServer(Forest, [grid], "Forest", {
-    "density": UserSettableParameter("slider","Tree density", 0.45, 0.01, 1.0, 0.01),
-    "width":50, "height":50
-})
+# Creación de tabla que grafica datacollector
+chart = ChartModule([{"Label": "Percent burned", "Color": "Black"}], data_collector_name='datacollector')
+
+# Implementación de Slider para manipular densidad de bosque
+server = ModularServer(Forest,[grid, chart],"Forest",
+                       {"density": UserSettableParameter(
+                            "slider", "Tree density", 0.45, 0.01, 1.0, 0.01),
+                            "width":20,
+                            "height":20})
 
 server.port = 8522 # The default
 server.launch()
