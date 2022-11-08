65f6799960f783d537f70dac37305fb07fb522ab
ale_wsl <alejo.ubeto@gmail.com>
2022-11-07 18:00:29 -0600

    P2: Slider para controlar densidad de bosque

--- Quizzes/fire.py
+++ Quizzes/fire.py
@@ -5,6 +5,9 @@ from mesa.time import RandomActivation
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
+from mesa.visualization.UserParam import UserSettableParameter
+
+# Clase Agente: Arbol
 class Tree(Agent):
     FINE = 0
     BURNING = 1
@@ -20,6 +23,7 @@ class Tree(Agent):
                     neighbor.condition = self.BURNING
             self.condition = self.BURNED_OUT
 
+# Clase Modelo: Forest
 class Forest(Model):
     def __init__(self, height=50, width=50, density=0.90):
         super().__init__()
@@ -36,6 +40,7 @@ class Forest(Model):
     def step(self):
         self.schedule.step()
 
+# Coloreado de agentes
 def agent_portrayal(agent):
     if agent.condition == Tree.FINE:
         portrayal = {"Shape": "circle", "Filled": "true", "Color": "Green", "r": 0.75, "Layer": 0}
@@ -49,7 +54,12 @@ def agent_portrayal(agent):
     return portrayal
 
 grid = CanvasGrid(agent_portrayal, 50, 50, 450, 450)
-server = ModularServer(Forest, [grid], "Forest", {})
+
+# Slider para manipular densidad de bosque
+server = ModularServer(Forest, [grid], "Forest", {
+    "density": UserSettableParameter("slider","Tree density", 0.45, 0.01, 1.0, 0.01),
+    "width":50, "height":50
+})
 
 server.port = 8522 # The default
 server.launch()
