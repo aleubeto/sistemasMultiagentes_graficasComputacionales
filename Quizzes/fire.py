de6b3f8aa2c4863e150972671414257f19605084
Rocha134 <rochapakito@gmail.com>
2022-11-09 17:01:17 -0600

    Avance extensión 3

--- Quizzes/fire.py
+++ Quizzes/fire.py
@@ -15,12 +15,13 @@ class Tree(Agent):
     FINE = 0
     BURNING = 1
     BURNED_OUT = 2
-    def __init__(self, model: Model, probability_of_spread, south_wind_speed, west_wind_speed):
+    def __init__(self, model: Model, probability_of_spread, south_wind_speed, west_wind_speed, big_jumps):
         super().__init__(model.next_id(), model)
         self.condition = self.FINE
         self.probability_of_spread = probability_of_spread
         self.south_wind_speed = south_wind_speed
         self.west_wind_speed = west_wind_speed
+        self.big_jumps = big_jumps
 
     def step(self):
         if self.condition == self.BURNING:
@@ -51,17 +52,39 @@ class Tree(Agent):
                 # Cambio de estado
                 if neighbor_fine and spread_rand < self.probability_of_spread and wind_condition:
                     neighbor.condition = self.BURNING
+
+                #We need to chceck if big jumps is enabled
+                if self.big_jumps == True:
+                    # Big Jump
+                    jumpX = int((self.pos[0] + self.west_wind_speed))
+                    jumpY = int((self.pos[1] + self.south_wind_speed))
+
+                    #We need to check if the jump is valid
+                    if jumpX >= 0 and jumpX < self.model.grid.width and jumpY >= 0 and jumpY < self.model.grid.height:
+                        #We need to check if the grid position contains a tree
+                        if self.big_jumps and self.model.grid.is_cell_empty((jumpX, jumpY)) == False:
+                            jumpTree = self.model.grid.get_cell_list_contents([{jumpX,jumpY}])[0]
+                            #We need to check if the tree is fine
+                            if jumpTree.condition == self.FINE:
+                                jumpTree.condition == self.BURNING
+
+            # Se apaga el arbol emisor
             self.condition = self.BURNED_OUT
 
 # Clase Modelo: Forest
+
+'''def getAgentFromModel(model, pos):
+                    for agent in model.schedule.agents:
+                        if agent.pos == pos:
+                            return agent'''
 class Forest(Model):
-    def __init__(self, height=50, width=50, density=0.90, probability_of_spread=50, south_wind_speed=0, west_wind_speed=0):
+    def __init__(self, height=50, width=50, density=0.90, probability_of_spread=50, south_wind_speed=0, west_wind_speed=0, big_jumps=False):
         super().__init__()
         self.schedule = RandomActivation(self)
         self.grid = Grid(height, width, torus=False)
         for _,x,y in self.grid.coord_iter():
             if self.random.random() < density:
-                tree = Tree(self, probability_of_spread, south_wind_speed, west_wind_speed)
+                tree = Tree(self, probability_of_spread, south_wind_speed, west_wind_speed, big_jumps)
                 if x == 0:
                     tree.condition = Tree.BURNING
                 self.grid.place_agent(tree, (x,y))
@@ -113,6 +136,8 @@ server = ModularServer(Forest,[grid, chart],"Forest",
                             "slider", "South wind speed", 0, -25, 25, 1),
                         "west_wind_speed": UserSettableParameter(
                             "slider", "West wind speed", 0, -25, 25, 1),
+                        "big_jumps": UserSettableParameter(
+                            "checkbox", "Big Jumps", False),
                         "width":50,
                         "height":50
                         })
