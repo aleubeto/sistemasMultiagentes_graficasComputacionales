073dcd2266b40ebe15890f17f3075aa6975e0c45
ale-argos <alejo.ubeto@gmail.com>
2022-11-18 16:18:11 -0600

    Inicio de test web server

---
+++ Actividades/wrapper22/backend/pacman.py
@@ -0,0 +1,36 @@
+from mesa import Agent, Model
+from mesa.space import Grid
+from mesa.time import RandomActivation
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+
+class Ghost(Agent):
+    def __init__(self, model, pos):
+        super().__init__(model.next_id(), model)
+        self.pos = pos
+    def step(self):
+        print(self.pos)
+        next_moves = self.model.grid.get_neighborhood(self.pos, moore=False)
+        next_move = self.random.choice(next_moves)
+        self.model.grid.move_agent(self, next_move)
+class Maze(Model):
+    def __init__(self):
+        super().__init__()
+        self.schedule = RandomActivation(self)
+        self.grid = Grid(17, 14, torus=False)
+
+        ghost = Ghost(self, (8, 6))
+        self.grid.place_agent(ghost, ghost.pos)
+        self.schedule.add(ghost)
+        
+    def step(self):
+        self.schedule.step()
+
+# def agent_portrayal(agent):
+#     return {"Shape": "ghost.png", "Layer": 0}
+
+# grid = CanvasGrid(agent_portrayal, 17, 14, 450, 450)
+
+# server = ModularServer(Maze, [grid], "PacMan", {})
+# server.port = 8523
+# server.launch()
