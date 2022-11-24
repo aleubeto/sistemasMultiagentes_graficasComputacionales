c183f85cd4986c539f7ae607d7b08fb6cf64d19f
Omar Jimenez <chitox_ji_armen@hotmail.com>
2022-11-24 09:38:17 -0600

    version con busqueda aleatorizada

--- Quizzes/robotAgent.py
+++ Quizzes/robotAgent.py
@@ -12,34 +12,46 @@ from pathfinding.core.grid import Grid as pathGrid
 from pathfinding.finder.a_star import AStarFinder
 
 class Robot(Agent):
-    BUSCANDOCAJA = 0
+    DEAMBULANDO = 0
     ENCAMINO = 1
-    BUSCANDOESTANTE = 2
+    REGRESARESTANTE = 2
     def __init__(self, model, pos, width, height):
         super().__init__(model.next_id(), model)
-        self.condition = self.BUSCANDOCAJA
+        self.condition = self.DEAMBULANDO
         self.pos = pos
         self.move_counter = 0
         self.w = width
         self.h = height
         self.sig = 1
         self.path = []
-        self.cargando = False
         self.activo = True
         self.carga = None
+        self.direccion = (0, 0)
+        self.hallazgo = None
 
     def step(self):
         if self.activo == False:
             return
         
-        if self.condition == self.BUSCANDOCAJA:
-            self.objetivo = self.encontrar_caja()
-            if self.objetivo == None:
-                self.activo = False
-            else:
-                self.path = self.pathfinding(self.objetivo)
+        if self.condition == self.DEAMBULANDO:
+            if self.hallazgo != None:
+                self.path = self.pathfinding(self.hallazgo)
+                self.sig = 1
                 self.condition = self.ENCAMINO
-        
+                
+            else:
+                self.encontrar_caja()
+
+                if self.carga == None:
+                    siguiente = (self.pos[0] + self.direccion[0], self.pos[1] + self.direccion[1])
+                    if self.model.grid.is_cell_empty(siguiente):
+                        self.model.grid.move_agent(self, siguiente)
+                    else:
+                        self.cambiar_direccion()
+                        for element in self.model.grid.get_cell_list_contents(siguiente):
+                            if type(element) == Robot:
+                                element.cambiar_direccion()
+
         elif self.condition == self.ENCAMINO:
             if self.sig < len(self.path):
                 paso = True
@@ -47,46 +59,62 @@ class Robot(Agent):
                     if type(element) == Robot:
                         element.model.grid.move_agent(element, self.path[self.sig - 1])
                         paso = False
+                        
                 if paso:
+                    self.model.grid.move_agent(self, self.path[self.sig])
                     if self.carga != None:
                         self.carga.model.grid.move_agent(self.carga, self.path[self.sig])
-                    self.model.grid.move_agent(self, self.path[self.sig])
+                    else:
+                        self.encontrar_caja()
                     self.sig = self.sig + 1
             else:
-                if self.cargando:
-                    self.condition = self.BUSCANDOCAJA
-                    self.cargando = False
+                if self.carga != None:
+                    self.condition = self.DEAMBULANDO
                     self.model.grid.remove_agent(self.carga)
                     self.model.schedule.remove(self.carga)
                     self.model.box_list.remove(self.carga)
+                    self.hallazgo = None
                     self.carga = None
+                    print("elimine una caja")
                 else:
-                    self.condition = self.BUSCANDOESTANTE
-                    self.cargando = True
-                    self.carga = self.objetivo
-                self.sig = 1
+                    self.condition = self.DEAMBULANDO
+                    self.hallazgo = None
             
-        elif self.condition == self.BUSCANDOESTANTE:
+        elif self.condition == self.REGRESARESTANTE:
             self.objetivo = self.encontrar_estante()
+            self.sig = 1
             if self.objetivo == None:
                 self.activo = False
             else:
-                self.path = self.pathfinding(self.objetivo)
+                self.path = self.pathfinding(self.objetivo.pos)
                 self.condition = self.ENCAMINO
-                #comportamiento de busqueda
+    
+    def cambiar_direccion(self):
+        random = self.random.randrange(0, 3, 1)
+        if random == 0:
+            self.direccion = (1, 0)
+        elif random == 1:
+            self.direccion = (0, 1)
+        elif random == 2:
+            self.direccion = (-1, 0)
+        elif random == 3:
+            self.direccion = (0, -1)
     
     def encontrar_caja(self):
-        menor = self.w * self.h
-        caja_menor = None
-        for caja in self.model.box_list:
-            distancia = ((caja.pos[0] - self.pos[0]) * (caja.pos[0] - self.pos[0])) + ((caja.pos[1] - self.pos[1]) * (caja.pos[1] - self.pos[1])) 
-            if distancia < menor and caja.condition == caja.DISPONIBLE:
-                menor = distancia
-                caja_menor = caja
-        if caja_menor != None:
-            caja_menor.condition = caja_menor.OCUPADA
-            
-        return caja_menor
+        buscando = True
+        for element in self.model.grid.neighbor_iter(self.pos, False):
+            if type(element) == Caja and buscando:
+                if element.condition == element.DISPONIBLE:
+                    buscando = False
+                    self.model.grid.move_agent(self, element.pos)
+                    element.condition = element.OCUPADA
+                    self.carga = element
+                    for robot in self.model.robot_list:
+                        if robot.condition == robot.DEAMBULANDO:
+                            robot.hallazgo = element.pos
+                    if self.condition == self.ENCAMINO:
+                        self.sig = len(self.path)
+                    self.condition = self.REGRESARESTANTE
     
     def encontrar_estante(self):
         mejor = 0
@@ -104,7 +132,7 @@ class Robot(Agent):
     def pathfinding(self, destino):
         grid = pathGrid(width=self.w, height=self.h, matrix=self.model.matrix)
         start = grid.node(self.pos[0], self.pos[1])
-        end = grid.node(destino.pos[0], destino.pos[1])
+        end = grid.node(destino[0], destino[1])
         finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
         path, runs = finder.find_path(start, end, grid)
         
@@ -157,6 +185,7 @@ class Room(Model):
         
         self.box_list = []
         self.shelf_list = []
+        self.robot_list = []
         
         self.grid = MultiGrid(self.w, self.h, torus=False)
         
@@ -215,6 +244,7 @@ class Room(Model):
                 robot = Robot(self, (x, y), self.w, self.h)
                 self.grid.place_agent(robot, robot.pos)
                 self.schedule.add(robot)
+                self.robot_list.append(robot)
         
         for i in range(boxes):
             x = self.random.randrange(0, self.w)
@@ -288,13 +318,13 @@ def agent_portrayal(agent):
 grid = CanvasGrid(agent_portrayal, 30, 30, 450, 450)
 
 # Creación de tabla que grafica datacollector
-chart_tiempo = ChartModule([{"Label": "Tiempo transcurrido", "Color": "Black"}], data_collector_name='time_datacollector')
+#chart_tiempo = ChartModule([{"Label": "Tiempo transcurrido", "Color": "Black"}], data_collector_name='time_datacollector')
+#
+#chart_cajas = ChartModule([{"Label": "Cantidad de cajas restante", "Color": "Black"}], data_collector_name='boxes_datacollector')
+#
+#chart_movimientos = ChartModule([{"Label": "Movimientos realizados", "Color": "Black"}], data_collector_name='move_datacollector')
 
-chart_cajas = ChartModule([{"Label": "Cantidad de cajas restante", "Color": "Black"}], data_collector_name='boxes_datacollector')
-
-chart_movimientos = ChartModule([{"Label": "Movimientos realizados", "Color": "Black"}], data_collector_name='move_datacollector')
-
-server = ModularServer(Room, [grid,chart_tiempo, chart_cajas, chart_movimientos], "Equipo 10 - M1. Actividad",
+server = ModularServer(Room, [grid], "Equipo 10 - M1. Actividad",
                         {"width": UserSettableParameter(
                             "number", "Anchura", 30),
                         "height": UserSettableParameter(
