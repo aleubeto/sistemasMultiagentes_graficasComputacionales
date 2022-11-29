6572f11e860edf5936954f6549e54f0cd764f591
Rocha134 <rochapakito@gmail.com>
2022-11-28 18:13:53 -0600

    Trying to fix cors anywhere

--- Integradora/Frontend/src/client/client.ts
+++ Integradora/Frontend/src/client/client.ts
@@ -13,6 +13,50 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 var firstFrame = true
 var robotsNumber = 0;
 
+//We create a variable path to store the path to the assets folder
+const path = 'https://github.com/aleubeto/sistemasMultiagentes_graficasComputacionales/blob/nocheDesvelo/Integradora/Frontend/assets/Robot/scene.gltf'
+
+//We add a 3D model GLTF to the scene from the assets folder
+async function addModel(x: number, y: number, z: number, scale: number, path: string) {
+    //We create a new GLTFLoader
+    const loader = new GLTFLoader()
+    //We load the model
+    loader.load(
+        //We pass the path to the model
+        path,
+        //We pass the function that will be executed after the model is loaded
+        function (gltf) {
+            //We get the model from the gltf object
+            const model = gltf.scene.children[0]
+            //We set the model position
+            model.position.set(x, y, z)
+            //We set the model scale
+            model.scale.set(scale, scale, scale)
+            //We add the model to the scene
+            scene.add(model)
+        },
+        //We pass the function that will be executed while the model is loading
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+        },
+        //We pass the function that will be executed if there is an error loading the model
+        function (error) {
+            console.log('An error happened')
+        }
+    )
+}
+
+addModel(0.5, 0.5, 0.5, 0.5, path)
+
+//We create a function to add a light to the scene
+function addLight(x: number, y: number, z: number) {
+    //We create a new directional light
+    const light = new THREE.DirectionalLight(0xffffff, 1)
+    //We set the light position
+    light.position.set(x, y, z)
+    //We add the light to the scene
+    scene.add(light)
+}
 
 
 var baseURL = "http://localhost:5000"
